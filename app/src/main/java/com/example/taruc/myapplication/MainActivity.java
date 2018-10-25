068d1066cc8d5b8df7278fa9c234a72f70b9c9a9
anreh <anurehka@gmail.com>
2018-10-25 13:42:18 +0800

    123

--- app/src/main/java/com/example/taruc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.taruc.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.example.taruc.myapplication.extra.MESSAGE";
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("MYOWN", "This is a debug level log");
         Log.w("BLAHBLAH", "This is a warning level log");
+        mMessageEditText = findViewById(R.id.editText_main);
+
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
 
     }
 }
